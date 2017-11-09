2a7e27d53417b4f556a9e164586eeec8fe8a04ab
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-09 09:54:34 +0100

    Added first steps

--- js/project.js
+++ js/project.js
@@ -1,3 +1,5 @@
+var StepOne;
+
 let currentStep = 0;
 let maxSteps = 4;
 
@@ -11,12 +13,48 @@ let maxSteps = 4;
   Listner.add('#nextStep', 'click', nextStep);
 })();
 
+function StepsEventListners() {
+  Listner.remove('#folder', 'keyup', function() { StepOne.folderLocation();});
+  Listner.remove("#cloneUrl", 'keyup', function(){StepOne.cloneURL();})
+  switch (currentStep) {
+    case 0:
+
+      break;
+    case 1:
+      Listner.add('#folder', 'keyup', function() { StepOne.folderLocation();});
+      Listner.add("#cloneUrl", 'keyup', function(){StepOne.cloneURL();})
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+  }
+}
+
+(function() {
+  StepOne = {
+    folderLocation: function() {
+      select("#zoekFolderCommando").innerHTML = "cd " + select("#folder").value;
+    },
+    cloneURL: function() {
+      select("#cloneCommand").innerHTML = "git clone " + select("#cloneUrl").value;
+    }
+  }
+})();
+
+
 function previousStep() {
   if (currentStep > 0) {
     // We can go back
     currentStep--;
     select("#step").innerHTML = Ajax.get_withCallback('steps/step' + currentStep + '.html').responseText;
     console.log("Back");
+    StepsEventListners();
   }
 
   else {
@@ -30,6 +68,7 @@ function nextStep() {
     console.log("Next");
     currentStep++;
     select("#step").innerHTML = Ajax.get_withCallback('steps/step' + currentStep + '.html').responseText;
+    StepsEventListners();
   }
 
   else {
